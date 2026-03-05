494b77e80ccbd4f0b0367cc8a4f581f677a31e1e
Glenn Hartmann <hartmann.glenn@gmail.com>
2026-03-05 18:04:16 -0500

    update build and test scripts
    not sure why or when this option is required, but it seems to be sometimes

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-go build github.com/glennhartmann/ledger-tools/src/transactionsorter
-go build github.com/glennhartmann/ledger-tools/src/pricedbfetcher
-go build github.com/glennhartmann/ledger-tools/src/questrademain
-go build github.com/glennhartmann/ledger-tools/src/pricedbtocsv
+go build -mod=readonly github.com/glennhartmann/ledger-tools/src/transactionsorter
+go build -mod=readonly github.com/glennhartmann/ledger-tools/src/pricedbfetcher
+go build -mod=readonly github.com/glennhartmann/ledger-tools/src/questrademain
+go build -mod=readonly github.com/glennhartmann/ledger-tools/src/pricedbtocsv
